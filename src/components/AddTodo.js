5735c60b873b06a8a225b6e491e1d5b06a73693c
Lyle Rubin <lylerubin912@gmail.com>
2021-03-27 12:44:55 -0800

    Added ability to add new tasks

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,20 +1,41 @@
 import React, { Component } from "react";
 
 export class AddTodo extends Component {
+  state = {
+    title: "",
+  };
+
+  onChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  onSubmit = (event) =>{
+    event.preventDefault();
+    this.props.addTodo(this.state.title)
+    this.setState({
+      title: ""
+    })
+  }
+
   render() {
     return (
-      <form style={{ display: "flex" }}>
+      <form onSubmit = {this.onSubmit} style={{ display: "flex" }}>
         <input
           type="text"
           name="title"
           placeholder="Add Todo"
-          style={{ flex: "10", padding: '5px' }}
+          style={{ flex: "10", padding: "5px" }}
+          value={this.state.title}
+          onChange={this.onChange}
         />
         <input
           type="submit"
           value="Add Todo"
           className="button"
           style={{ flex: "1" }}
+  
         />
       </form>
     );
